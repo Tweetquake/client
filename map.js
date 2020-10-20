454e5d3bdfac87a17ad05ae40ba4e75bbe10f6e4
Lorenza Pasquini <lorenza.pasquini@hotmail.it>
2020-10-20 18:34:25 +0200

    add provincial_capitals GEOJSON

--- map.js
+++ map.js
@@ -9,11 +9,15 @@ require(["esri/Map", "esri/views/MapView", "esri/widgets/BasemapToggle",
 ) {
     // Create GeoJSONLayer for the area at risk
     var geojsonLayer_area = new GeoJSONLayer({
-        url: "areaAtRisk.geojson" //or try "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+        url: "areaAtRisk.geojson"
     });
     // Create GeoJSONLayer for the seismogenic sources
     var geojsonLayer_sources = new GeoJSONLayer({
-        url: "seismogenicSources.geojson" //or try "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+        url: "seismogenicSources.geojson"
+    });
+    // Create GeoJSONLayer for the provincial capitals
+    var geojsonLayer_capitals = new GeoJSONLayer({
+        url: "provincial_capitals.geojson"
     });
 
     // Create GeoJSONLayer for the seismogenic sources
@@ -29,7 +33,7 @@ require(["esri/Map", "esri/views/MapView", "esri/widgets/BasemapToggle",
     // Create the Map with an initial basemap
     var map = new Map({
         basemap: "hybrid",
-        layers: [geojsonLayer_area,geojsonLayer_sources,geojsonLayer_road_nodes,geojsonLayer_road_edges]
+        layers: [geojsonLayer_area,geojsonLayer_sources,geojsonLayer_capitals,geojsonLayer_road_nodes,geojsonLayer_road_edges]
     });
 
     // Create the MapView and reference the Map in the instance
